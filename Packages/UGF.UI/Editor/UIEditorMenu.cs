0b7506113fd9c513373a6eaa94bf8b74c56b608c
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-06 22:05:14 +0300

    Add UIEditorMenu

---
+++ Packages/UGF.UI/Editor/UIEditorMenu.cs
@@ -0,0 +1,29 @@
+﻿using UGF.UI.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.UI.Editor
+{
+    internal static class UIEditorMenu
+    {
+        [MenuItem("CONTEXT/RectTransform/Anchors To Corners", false, 2000)]
+        private static void RectTransformAnchorsToCorners(MenuCommand menuCommand)
+        {
+            var rectTransform = (RectTransform)menuCommand.context;
+
+            Undo.RecordObject(rectTransform, "Anchors To Corners");
+            UIUtility.SetAnchorsToCorners(rectTransform);
+            EditorUtility.SetDirty(rectTransform);
+        }
+
+        [MenuItem("CONTEXT/RectTransform/Corners To Anchors", false, 2000)]
+        private static void RectTransformCornersToAnchors(MenuCommand menuCommand)
+        {
+            var rectTransform = (RectTransform)menuCommand.context;
+
+            Undo.RecordObject(rectTransform, "Corners To Anchors");
+            UIUtility.SetCornersToAnchors(rectTransform);
+            EditorUtility.SetDirty(rectTransform);
+        }
+    }
+}
