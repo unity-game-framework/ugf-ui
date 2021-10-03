f69e77e66436e6d14e5b7a86ba27376039b5b901
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-10-03 20:59:15 +0300

    Update UIUtility.cs

--- Packages/UGF.UI/Runtime/UIUtility.cs
+++ Packages/UGF.UI/Runtime/UIUtility.cs
@@ -12,16 +12,16 @@ namespace UGF.UI.Runtime
             if (rectTransform.parent is RectTransform parent)
             {
                 Rect rect = parent.rect;
-                Vector2 anchorsMin = Vector2.zero;
-                Vector2 anchorsMax = Vector2.zero;
+                Vector2 min = Vector2.zero;
+                Vector2 max = Vector2.zero;
 
-                anchorsMin.x = rectTransform.anchorMin.x + rectTransform.offsetMin.x / rect.width;
-                anchorsMin.y = rectTransform.anchorMin.y + rectTransform.offsetMin.y / rect.height;
-                anchorsMax.x = rectTransform.anchorMax.x + rectTransform.offsetMax.x / rect.width;
-                anchorsMax.y = rectTransform.anchorMax.y + rectTransform.offsetMax.y / rect.height;
+                min.x = rectTransform.anchorMin.x + rectTransform.offsetMin.x / rect.width;
+                min.y = rectTransform.anchorMin.y + rectTransform.offsetMin.y / rect.height;
+                max.x = rectTransform.anchorMax.x + rectTransform.offsetMax.x / rect.width;
+                max.y = rectTransform.anchorMax.y + rectTransform.offsetMax.y / rect.height;
 
-                rectTransform.anchorMin = anchorsMin;
-                rectTransform.anchorMax = anchorsMax;
+                rectTransform.anchorMin = min;
+                rectTransform.anchorMax = max;
                 rectTransform.offsetMin = Vector2.zero;
                 rectTransform.offsetMax = Vector2.zero;
             }
