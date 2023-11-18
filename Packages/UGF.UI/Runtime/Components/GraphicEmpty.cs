126d31d95299f44a6da6b57efd92757219f8bb38
Vorobey <vorobyovvorobyov@gmail.com>
2023-11-18 16:08:10 +0300

    Add GraphicEmpty

---
+++ Packages/UGF.UI/Runtime/Components/GraphicEmpty.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UGF.UI.Runtime.Components
+{
+    [RequireComponent(typeof(CanvasRenderer))]
+    [AddComponentMenu("Unity Game Framework/UI/Graphic Empty", 2000)]
+    public class GraphicEmpty : Graphic
+    {
+        public override void SetVerticesDirty()
+        {
+        }
+
+        public override void SetMaterialDirty()
+        {
+        }
+
+        protected override void UpdateMaterial()
+        {
+        }
+
+        protected override void UpdateGeometry()
+        {
+        }
+
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+        }
+
+        public override void CrossFadeColor(Color targetColor, float duration, bool ignoreTimeScale, bool useAlpha)
+        {
+        }
+
+        public override void CrossFadeColor(Color targetColor, float duration, bool ignoreTimeScale, bool useAlpha, bool useRGB)
+        {
+        }
+
+        public override void CrossFadeAlpha(float alpha, float duration, bool ignoreTimeScale)
+        {
+        }
+    }
+}
